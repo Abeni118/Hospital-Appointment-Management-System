12fc6d08f6838d885991d8831304c975c0471ad4
Bernabas7777 <barniderese@gmail.com>
2026-01-29 14:30:00 +0300

    Create Appointment class with appointment methods

---
+++ Appointment.java
@@ -0,0 +1,25 @@
+public class Appointment {
+
+    private int appointmentId;
+    private String appointmentDate;
+    private String appointmentTime;
+    private String reason;
+
+    public void scheduleAppointment() {
+    }
+
+    public void cancelAppointment() {
+    }
+
+    public void approveAppointment() {
+    }
+
+    public void rejectAppointment() {
+    }
+
+    public void rescheduleAppointment() {
+    }
+
+    public void updateStatus() {
+    }
+}
