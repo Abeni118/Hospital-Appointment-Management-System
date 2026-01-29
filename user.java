94f56348e8f741f634e4ebb0d1e0a270df04d22e
Abenezer Tekalign <abenezertekalign118@gmail.com>
2026-01-29 12:39:08 +0300

    Create user.java

---
+++ user.java
@@ -0,0 +1,21 @@
+public class User {
+
+    protected int userId;
+    protected String username;
+    protected String password;
+    protected String email;
+    protected String phone;
+    protected String role;
+
+    public void login() {
+    }
+
+    public void logout() {
+    }
+
+    public void updateProfile() {
+    }
+
+    public void changePassword() {
+    }
+}
