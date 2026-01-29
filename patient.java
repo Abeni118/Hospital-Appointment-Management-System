1c6e890e48745cbbbc56553466146772b46c2cde
abeni080 <abenezeramenu82@gmail.com>
2026-01-29 13:33:17 +0300

    Create Patient class extending User with methods

---
+++ patient.java
@@ -0,0 +1,25 @@
+public class Patient extends User {
+
+    private int patientId;
+    private String dateOfBirth;
+    private String gender;
+    private String address;
+
+    public void bookAppointment() {
+    }
+
+    public void cancelAppointment() {
+    }
+
+    public void viewAppointment() {
+    }
+
+    public void viewBookingDetails() {
+    }
+
+    public void selectDoctorPreference() {
+    }
+
+    public void receiveNotification() {
+    }
+}
