e53f2c00ae5f6c3297c62595dd2144173cda0b5a
Abenezer Tekalign <abenezertekalign118@gmail.com>
2026-01-29 15:06:17 +0300

    Add Booking class with basic methods

---
+++ code/booking.java
@@ -0,0 +1,19 @@
+public class Booking {
+
+    private int bookingId;
+    private String bookingDate;
+    private String bookingStatus;
+    private String remarks;
+
+    public void createBooking() {
+    }
+
+    public void confirmBooking() {
+    }
+
+    public void cancelBooking() {
+    }
+
+    public void updateBookingStatus() {
+    }
+}
