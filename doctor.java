c384a9c2b6000b97851f334a131306bda3df8964
abeni080 <abenezeramenu82@gmail.com>
2026-01-29 13:34:44 +0300

    Add Doctor class with appointment management methods

---
+++ doctor.java
@@ -0,0 +1,22 @@
+public class Doctor extends User {
+
+    private int doctorId;
+    private String specialization;
+    private String qualification;
+    private String department;
+
+    public void viewSchedule() {
+    }
+
+    public void approveAppointment() {
+    }
+
+    public void rejectAppointment() {
+    }
+
+    public void updateAvailability() {
+    }
+
+    public void viewPatientDetails() {
+    }
+}
