81d4185883ca7d9c1e11d3ebae53ebf11d980b2e
Abenezer Tekalign <abenezertekalign118@gmail.com>
2026-01-29 15:08:47 +0300

    Create Hospital class with basic methods

---
+++ code/hospital.java
@@ -0,0 +1,24 @@
+public class Hospital {
+
+    private String hospitalId;
+    private String hospitalName;
+    private String address;
+    private String phoneNumber;
+    private String email;
+    private String operatingHours;
+
+    public void addDoctor() {
+    }
+
+    public void removeDoctor() {
+    }
+
+    public void addNurse() {
+    }
+
+    public void getAvailableDoctors() {
+    }
+
+    public void getHospitalDetails() {
+    }
+}
