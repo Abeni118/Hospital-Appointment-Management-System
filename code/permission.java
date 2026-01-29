3f9352b6bd07f1594ad0b8c3f88377eb9dfaccf7
Abenezer Tekalign <abenezertekalign118@gmail.com>
2026-01-29 15:07:34 +0300

    Add Permission class with basic methods

---
+++ code/permission.java
@@ -0,0 +1,15 @@
+public class Permission {
+
+    private int permissionId;
+    private String permissionName;
+    private String description;
+
+    public void grantPermission() {
+    }
+
+    public void revokePermission() {
+    }
+
+    public void getPermissionDetails() {
+    }
+}
