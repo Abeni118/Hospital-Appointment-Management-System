3fd0599bf981c8061cd4ae4ea5bcd565b74adf74
Bernabas7777 <barniderese@gmail.com>
2026-01-29 14:32:17 +0300

    Create Role class with basic methods

---
+++ role.java
@@ -0,0 +1,15 @@
+public class Role {
+
+    private int roleId;
+    private String roleName;
+    private String description;
+
+    public void assignRole() {
+    }
+
+    public void removeRole() {
+    }
+
+    public void getRoleDetails() {
+    }
+}
