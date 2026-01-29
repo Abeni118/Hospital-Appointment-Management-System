3459d7c2394539aedd73a5dcafee2c137c948325
abeni080 <abenezeramenu82@gmail.com>
2026-01-29 13:41:09 +0300

    Create Admin class extending User with methods

---
+++ Admin.java
@@ -0,0 +1,24 @@
+public class Admin extends User {
+
+    private int adminId;
+    protected String officeLocation;
+    protected String accessLevel;
+
+    public void manageDoctors() {
+    }
+
+    public void manageNurses() {
+    }
+
+    public void manageDepartments() {
+    }
+
+    public void assignRolesToUsers() {
+    }
+
+    public void manageRoles() {
+    }
+
+    public void managePermissions() {
+    }
+}
